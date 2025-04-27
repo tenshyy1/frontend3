242c11cba65de2c494aa1933924a18b845290cca
tenshyy1 <arturtk000@gmail.com>
2025-04-27 18:46:29 +0300

    123

--- front/storage.js
+++
@@ -1,8 +0,0 @@
-function saveTasks(tasks) {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }
-  
-  function loadTasks() {
-    return JSON.parse(localStorage.getItem('tasks')) || [];
-  }
-  
